c4c484530258f9dc56c1e3c770bee81246397768
David Chay <yapperdark2@gmail.com>
2024-09-04 19:45:53 -0600

    feat: Add input fields for triangle dimensions

--- javascript/fuctions.js
+++ javascript/fuctions.js
@@ -70,6 +70,7 @@ $(document).ready(function () {
                         <input type="number" class="form-control" id="height" step="0.01" required>
                     </div>
                 `);
+                break;
             case 'trianIsosceles':
                 $('#figureImage').html('<svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-triangle-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"/></svg>');
                 $('#inputFields').append(`
@@ -86,6 +87,7 @@ $(document).ready(function () {
                         <input type="number" class="form-control" id="sides" step="0.01" required>
                     </div>
                 `);
+                break;
         }
 
         $('#inputFields').show();
